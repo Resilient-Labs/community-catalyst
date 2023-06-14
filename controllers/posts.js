42b1d2e4ca6305d0ee560602db1455e3b7c0ae19
jevogithub <johnjevora94@gmail.com>
2023-06-14 18:43:06 -0400

    added events ejs and corrected routes to get to page ticket #62

--- controllers/posts.js
+++ controllers/posts.js
@@ -23,8 +23,7 @@ module.exports = {
   getPost: async (req, res) => {
     try {
       const post = await Post.findById(req.params.id);
-      ({ createdAt: "desc" }).lean();
-      res.render("post.ejs", { post: post, user: req.user });
+      res.render("events.ejs", { post: post, user: req.user });
     } catch (err) {
       console.log(err);
     }
